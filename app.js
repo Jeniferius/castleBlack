403280449d9faf65e22ec6196734d82c2404bfaa
DESKTOP-HPP1BS6\Jeniferius <jenifeirus018@gmail.com>
2019-11-01 11:41:51 +0100

    pretty syntax

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const bodyParser = require("body-parser");
 const consola = require("consola");
 const router = require("./src/router.js");
 const api = require("./src/api.js");
-var path = require('path');
-var logger = require('morgan');
-const moment = require('moment');
-var cookieParser = require('cookie-parser');
+var path = require("path");
+var logger = require("morgan");
+const moment = require("moment");
+var cookieParser = require("cookie-parser");
 
 const app = express();
 const host = process.env.HOST || "0.0.0.0";
@@ -15,13 +15,13 @@ const port = process.env.PORT || 8080;
 app.set("port", port);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 async function run() {
   app.disable("x-powered-by"); // QUESTION: any reason is this line here?
@@ -40,8 +40,8 @@ async function run() {
   });
 }
 
-app.get('/', (req, res) => {
-  res.send('Castle Black');
+app.get("/", (req, res) => {
+  res.send("Castle Black");
 });
 
 run();
