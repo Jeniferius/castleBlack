5eb7e6cdbde19f9de9b84f053a17e0eddcf7977b
DESKTOP-HPP1BS6\Jeniferius <jenifeirus018@gmail.com>
2019-11-01 11:09:15 +0100

    add view engine setup

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const bodyParser = require("body-parser");
 const consola = require("consola");
 const router = require("./src/router.js");
 const api = require("./src/api.js");
+var path = require('path');
+var logger = require('morgan');
+const moment = require('moment');
+var cookieParser = require('cookie-parser');
 
 const app = express();
 const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 8080;
 app.set("port", port);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 async function run() {
   app.disable("x-powered-by"); // QUESTION: any reason is this line here?
@@ -26,5 +39,9 @@ async function run() {
     badge: true
   });
 }
+
+app.get('/', (req, res) => {
+  res.send('Castle Black');
+});
 
 run();
